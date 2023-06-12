5839e478d7c935dd7c332615c56891f82ecd4d87
Shuaib Abdul Khader <115252366+f00571164@users.noreply.github.com>
2023-06-12 06:03:40 +0300

    Create player-list.component.ts

---
+++ src/app/player-list/player-list.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { PlayerService } from '../player.service';
+import { Player } from '../player';
+
+@Component({
+  selector: 'app-player-list',
+  templateUrl: './player-list.component.html',
+  styleUrls: ['./player-list.component.css']
+})
+export class PlayerListComponent implements OnInit {
+
+  players: Player[];
+
+  constructor(private playerService: PlayerService) { }
+
+  ngOnInit() {
+    this.getPlayers();
+  }
+
+  getPlayers(): void {
+    this.playerService.getPlayers().subscribe(players => this.players = players);
+  }
+
+}
