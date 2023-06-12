76df1610e77849afe0dff1432d2aee95b8d3e092
Shuaib Abdul Khader <115252366+f00571164@users.noreply.github.com>
2023-06-12 06:01:40 +0300

    Update player-add.component.ts

--- src/app/player-add/player-add.component.ts
+++ src/app/player-add/player-add.component.ts
@@ -1,1 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { PlayerService } from '../player.service';
+import { Player } from '../player';
+
+@Component({
+  selector: 'app-player-add',
+  templateUrl: './player-add.component.html',
+  styleUrls: ['./player-add.component.css']
+})
+export class PlayerAddComponent implements OnInit {
+
+  player: Player = new Player();
+
+  constructor(private playerService: PlayerService) { }
+
+  ngOnInit() {
+  }
+
+  addPlayer(): void {
+    this.playerService.addPlayer(this.player).subscribe(player => console.log(player));
+  }
+
+}
 
